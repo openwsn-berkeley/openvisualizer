bc4655b7a044ca7346d3fbf28d69100476b7bd13
Timothy Claeys <timothy.claeys@gmail.com>
2020-01-14 13:12:41 +0100

    Removes unused and faulty class method from BspModule
    - also formatted file according to PEP 8

--- openvisualizer/BspEmulator/BspModule.py
+++ openvisualizer/BspEmulator/BspModule.py
@@ -6,32 +6,26 @@
 # https://openwsn.atlassian.net/wiki/display/OW/License
 import logging
 
+
 class BspModule(object):
-    '''
+    """
     Emulates the 'board' BSP module
-    '''
-    
-    def __init__(self,name):
-        
+    """
+
+    def __init__(self, name):
         # store params
-        
+
         # local variables
         self.isInitialized = False
-        
+
         # logging
-        self.log  = logging.getLogger(name+'_'+str(self.motehandler.getId()))
+        self.log = logging.getLogger(name + '_' + str(self.motehandler.getId()))
         self.log.setLevel(logging.DEBUG)
         self.log.addHandler(logging.NullHandler())
-    
-    #======================== public ==========================================
-    
-    #=== getters
-    
-    def getIsInitialized():
-       return self.isInitialized
-    
-    #======================== private =========================================
-    
-    def _checkInit(self):
+
+    # ======================== public ==========================================
+
+    # ======================== private =========================================
+
+    def _check_init(self):
         assert self.isInitialized
-    
